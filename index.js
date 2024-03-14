cedff3d483a2fb8f91123907527395be42948894
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-03-14 18:22:39 +0530

    img url updated

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   // This is a simple example, you might want to dynamically generate this tag
   const ogImage =
-    "https://www.google.com/imgres?q=farcaste%20image&imgurl=https%3A%2F%2Fcdn.finex.cz%2Fwp-content%2Fuploads%2Ffinex-og%2Fpost%2F127456_square.jpg%3Ft%3D1707338692&imgrefurl=https%3A%2F%2Ffinex.cz%2Ffarcaster-bude-prvni-kryptomenovou-socialni-siti-ktera-se-mozna-konecne-chytne%2F&docid=fH37h7DhZANSGM&tbnid=8NkWPFPyX2XrcM&vet=12ahUKEwi7vvz14_OEAxWXTWcHHb5uCMcQM3oECGgQAA..i&w=1280&h=1280&hcb=2&ved=2ahUKEwi7vvz14_OEAxWXTWcHHb5uCMcQM3oECGgQAA";
-  const metaTag = `<meta property="og:image" content="${ogImage}" />`;
+    "https://wallpapers.com/images/featured-full/iron-man-ouqxo5w2b59h0042.jpg";
 
   res.send(`
     <!DOCTYPE html>
